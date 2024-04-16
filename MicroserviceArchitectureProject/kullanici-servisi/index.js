d439cc882c4bec9ddec072d68aa4d0dc34bc4f46
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-04-16 20:06:55 +0300

    Added UserService Project

---
+++ MicroserviceArchitectureProject/kullanici-servisi/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = 3001;
+// Örnek kullanıcı veritabanı
+let users = [];
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     description: Tüm kullanıcıları getirir
+ *     responses:
+ *       200:
+ *         description: Başarılı işlem
+ */
+
+// Tüm kullanıcıları listeleme
+app.get('/users', (req, res) => {
+    try {
+        console.log('Kullanıcılar listeleniyor');
+        res.json(users);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.post('/users', (req, res) => {
+    try{
+        console.log('Kullanıcı ekleme isteği alındı');
+        const { username, email } = req.body;
+        const newUser = { id: users.length + 1, username, email };
+        users.push(newUser);
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+app.listen(port, () => {
+    console.log(`Kullanıcı Servisi http://127.0.0.1:${port} adresinde çalışıyor`);
+});
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+});
