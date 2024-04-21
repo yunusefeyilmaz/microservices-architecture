8f5c89d3d656f3e0c8543e127ab9cfeb959ed91d
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-04-21 19:12:47 +0300

    Update author-service to use mysql-connector-python and python-dotenv

--- MicroserviceArchitectureProject/author-service/DAAuthor.py
+++ MicroserviceArchitectureProject/author-service/DAAuthor.py
@@ -1,36 +1,39 @@
-from flaskext.mysql import MySQL
+import mysql.connector
 import os
+from dotenv import load_dotenv
+load_dotenv()
 class DAAuthor:
     def __init__(self, app):
         self.app = app
         self.init_app()
-        self.mysql = MySQL()
-        self.mysql.init_app(app)
 
     def init_app(self):
-        self.app.config['MYSQL_DATABASE_HOST'] = os.getenv('DB_HOST')
-        self.app.config['MYSQL_DATABASE_USER'] = os.getenv('DB_USER')
-        self.app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('DB_PASSWORD')
-        self.app.config['MYSQL_DATABASE_DB'] = os.getenv('DB_NAME')
-        self.app.config['MYSQL_DATABASE_PORT'] = os.getenv('DB_PORT')
+        self.mydb = mysql.connector.connect(
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME'),
+            port=os.getenv('DB_PORT'),
+            auth_plugin='mysql_native_password'
+        )
 
     def get_authors(self):
         try:
-            cur = self.mysql.get_db().cursor()
+            cur = self.mydb.cursor()
             cur.execute("SELECT * FROM authors")
             authors = cur.fetchall()
             cur.close()
             return authors
         except Exception as e:
             print(e)
-            return {'error': 'Internal Server Error'}
+            return {'error': str(e), 'message': 'Error getting authors'}
 
     def add_author(self, name, nationality):
         try:
             author = {'name': name, 'nationality': nationality}
-            cur = self.mysql.get_db().cursor()
+            cur = self.mydb.cursor()
             cur.execute("INSERT INTO authors (name,nationality) VALUES (%s,%s)", (name,nationality))
-            self.mysql.get_db().commit()
+            self.mydb.commit()  
             cur.close()
             return author
         except Exception as e:
