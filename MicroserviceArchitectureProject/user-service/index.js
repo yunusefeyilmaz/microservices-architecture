d81852338f17b185bee8c41b180ccf7fad6ddc64
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-04-23 16:24:27 +0300

    Refactor user-service to use async/await for API endpoints

--- MicroserviceArchitectureProject/user-service/index.js
+++ MicroserviceArchitectureProject/user-service/index.js
@@ -1,42 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3001;
 
-const DAuser = require('./DAUser');
+const DAuser = require("./DAUser");
 
 // Get all users
-app.get('/users', async (req, res) => {
-    try {
-        console.log('List users request received');
-        const users = await DAuser.getUsers();
-        res.json(users);
-    } catch (error) {
-        console.log(error);
-    }
+app.get("/users", async (req, res) => {
+  try {
+    console.log("List users request received");
+    const users = await DAuser.getUsers();
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Add a new user
-app.post('/users', async (req, res) => {
-    try{
-        console.log('Add user request received');
-        const { username, email } = req.body;
-        const newUser = { id: users.length + 1, username, email };
-        await DAuser.addUser(username, email);
-        res.status(201).json(newUser);
-    } catch (error) {
-        console.log(error);
-    }
-
+app.post("/users", async (req, res) => {
+  try {
+    console.log("Add user request received");
+    const { username, email } = req.body;
+    const newUser = await DAuser.addUser(username, email);
+    res.status(200).json(newUser);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Listen
 app.listen(port, () => {
-    console.log(`User service listening at http://localhost:${port}`);
+  console.log(`User service listening at http://localhost:${port}`);
 });
 
-process.on('uncaughtException', function (err) {
-    console.log(err);
+process.on("uncaughtException", function (err) {
+  console.log(err);
 });
